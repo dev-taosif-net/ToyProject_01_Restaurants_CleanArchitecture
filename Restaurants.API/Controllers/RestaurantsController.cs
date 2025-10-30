c21baa1d8e2050d1758ebae0a6fc9969ec6a91a0
taosif <dev.taosif.net@gmail.com>
2025-10-30 15:51:29 +0600

    Refactor RestaurantsController to use MediatR
    Replaced direct service calls in `RestaurantsController` with MediatR to implement the Mediator pattern. Added `GetAllRestaurantsQuery` and its handler to manage the retrieval of restaurant data. Updated dependency injection to include `IMediator` and introduced AutoMapper for mapping domain entities to DTOs. These changes improve separation of concerns, modularity, and maintainability.

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -1,25 +1,23 @@
-﻿using FluentValidation;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Features.Restaurants;
 using Restaurants.Application.Features.Restaurants.Commands;
-using Restaurants.Application.Features.Restaurants.Dtos;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Restaurants.Application.Features.Restaurants.Queries;
 
 namespace Restaurants.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RestaurantsController(IRestaurantService restaurantService , IMediator mediator) : ControllerBase
+    public class RestaurantsController(IRestaurantService restaurantService, IMediator mediator) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> GetAllRestaurants()
         {
-            var restaurants = await restaurantService.GetAllRestaurantsAsync();
+            var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
             return Ok(restaurants);
         }
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetRestaurantById([FromRoute]int id)
+        public async Task<IActionResult> GetRestaurantById([FromRoute] int id)
         {
             var restaurant = await restaurantService.GetRestaurantByIdAsync(id);
 
---
+++ Restaurants.Application/Features/Restaurants/Queries/GetAllRestaurantsQuery.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Features.Restaurants.Dtos;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Features.Restaurants.Queries;
+
+public record GetAllRestaurantsQuery : IRequest<IEnumerable<RestaurantDto>>;
+
+public class GetAllRestaurantsQueryHandler(ILogger<GetAllRestaurantsQueryHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository)
+    : IRequestHandler<GetAllRestaurantsQuery, IEnumerable<RestaurantDto>>
+{
+    public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Getting all restaurants");
+
+        var restaurants = await restaurantsRepository.GetAllAsync();
+
+        //var restaurantDtos = restaurants.Select(x => x.ToRestaurantDto());  //Manual
+        var restaurantDtos = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+
+        return restaurantDtos;
+    }
+
+}
+
